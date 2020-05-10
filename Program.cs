2d6c5a084cd8a0e4a99d0c3b2f8630177a6343b8
kamilko250 <freshmultikk21@gmail.com>
2020-05-10 23:43:51 +0200

    Prosta implementacja

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Xml;
+
+
+namespace Dev23
+{
+    class Program
+    {
+       
+            
+
+        static void Main(string[] args)
+        {
+            XmlTextReader data = new XmlTextReader("data.xml");
+            
+                    int b = 0;
+            
+                while (data.Read())
+                {
+                    if (data.NodeType == XmlNodeType.Element)
+                    {
+                    if (data.Name == "data")
+                        b++;
+                    if (b == 10)
+                        return;
+
+                    if (data.HasAttributes)
+                    {
+                        string sha = "";
+                        string guid = "";
+                        sha = data.GetAttribute("sha256");
+                        guid = data.GetAttribute("guid");
+                        string value = data.Value;
+                        byte[] bytevalue = Encoding.ASCII.GetBytes(value);
+                        byte[] byteshavalue = Encoding.ASCII.GetBytes(sha);
+                        using (SHA256 mysha256 = SHA256.Create())
+                        {
+                            byte[] hasValue = mysha256.ComputeHash(bytevalue);
+               
+               
+                            if (byteshavalue.SequenceEqual(hasValue))
+                            {
+                                Console.WriteLine(guid);
+                            }
+               
+                        }
+
+                    }
+
+                    
+                }
+            }
+        }
+    }
+}
